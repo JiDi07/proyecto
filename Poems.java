c0a18eb9a407f7425c16d218bddf3229258f857c
Diana Acosta <diana.acostaolivares@gmail.com>
2025-04-29 12:15:25 +0200

    NUEVA LINEA

---
+++ Poems.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Poems {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Todo pasa por algo");
+
+    }
+}
